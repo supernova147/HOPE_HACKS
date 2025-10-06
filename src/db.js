1b51643027a2968bedd8eb21e84a9d90e5fcefae
Eddie Millsaps Jr <164912904+EddieMJr@users.noreply.github.com>
2025-10-06 19:26:20 -0400

    10/6/25 Deployed & Connected DB to Application & Completed styling for home and map pages

--- src/db.js
+++ src/db.js
@@ -1,22 +1,39 @@
+require ('dotenv').config({ path: require('path').join(__dirname, '..', '.env') }); // Helps db.js find .env file
+
+// [ Testing .env Connection ]
+// console.log('env Test:', {
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   name: process.env.DB_NAME
+// });
+
 const mysql = require("mysql2");
+const fs = require ('fs');
+const path = require ('path');
 
 const dbConnection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'HopeHacks3', // Eddie filled this out ;)
-  password: 'password',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME, // Eddie filled this out ;)
+  password: process.env.DB_PW,
+  ssl: { 
+    ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
+    rejectUnauthorized: true }
 });
 
 dbConnection.connect((err) => {
     if (err) throw err;
-    console.log('Connected to MySQL');
+    console.log('Connected to 1st-Party API! Hallelujah!!');
 })
 
-// dbConnection [TESTED CONNECTION]
-//     .promise()
-//     .query('SELECT * FROM personInfo')
-//     .then(([rows, fields]) => {
-//         console.log(rows)
-//     })
+/*[TESTED CONNECTION]*/
+dbConnection
+    .promise()
+    .query('SHOW TABLES;')
+    .then(([rows, fields]) => {
+        console.log('Tables:', rows)
+    })
+    .catch(err => console.error(err));
 
 module.exports = dbConnection;
