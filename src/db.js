2d9d8402b45c5fc38df90f91d8980085ad5661d6
Marlette Dakiwas <mjessdakiwas@gmail.com>
2025-10-07 09:19:46 -0400

    [UPDATE] Fetched, validated, and rendered address data from the database

--- src/db.js
+++ src/db.js
@@ -1,13 +1,16 @@
 /* Database Connection */
 require('dotenv').config(); // Have to install dotenv in order to use variables inside .env file
 
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 
-    const pool = mysql.createPool({ // Connection to DB - changed to pool 
-      host: 'localhost',  
-      user: 'root',  
-      database: 'HopeHacks3', // Eddie filled this out ;)
-      password: 'password',
-    }).promise(); 
+const pool = mysql
+    .createPool({
+        // Connection to DB - changed to pool
+        host: 'localhost',
+        user: 'root',
+        database: 'HopeHacks3', // Eddie filled this out ;)
+        password: 'password',
+    })
+    .promise();
 
 module.exports = { pool };
